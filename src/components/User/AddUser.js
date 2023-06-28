76438d12ddf24efe40e1e97b66500e4fcae6f5e3
Johnny Wells <johnnyw01github@gmail.com>
2023-06-28 09:19:41 -0500

    Added css modules to the adduser, card, and button components for styling.

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+import Button from "../UI/Button";
 
 const AddUser = props => {
 
@@ -7,15 +10,15 @@ const AddUser = props => {
   };
 
   return (
-    <div>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <Button type='submit'>Add User</Button>
       </form>
-    </div>
+    </Card>
   );
 
 };
