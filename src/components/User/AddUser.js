c43b50e439caee31d416f39d43b4e160cd7d228b
Johnny Wells <john.wells025@gmail.com>
2023-06-28 15:05:08 -0500

    Set and managed user usestate changes in the adduser.js file to capture the information the user enters into the form. Also set the logic to handele clearing the form after the user hits the 'Add User' buttom, as well as form validation so that the user cannot submit a form with missing or invalid input.

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -2,20 +2,40 @@ import React from "react";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
+import { useState } from "react";
 
 const AddUser = props => {
 
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredUserAge, setEnteredUserAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredUserAge);
+    setEnteredUsername('');
+    setEnteredUserAge('');
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const userAgeChangeHandler = (event) => {
+    setEnteredUserAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" value={enteredUsername} onChange={userNameChangeHandler}></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" value={enteredUserAge} onChange={userAgeChangeHandler}></input>
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
