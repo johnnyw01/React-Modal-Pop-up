51595ddef249b60566db66a80ad276f2a6054fdd
Johnny Wells <johnnyw01github@gmail.com>
2023-06-28 08:54:45 -0500

    Created the UI and User component folders. Created the form for adding a user in the AddUser.js file.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import AddUser from './components/User/AddUser';
 
 const App = () => {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
